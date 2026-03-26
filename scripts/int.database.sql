7a07bd55ad898dadf93a393e225a72004fa5bfde
indarjeet7 <indarjeetyadav509@gmail.com>
2026-03-26 02:59:26 -0700

    Update int.database.sql

--- scripts/int.database.sql
+++ scripts/int.database.sql
@@ -16,7 +16,16 @@ WARNING:
 DROP DATABASE IF EXISTS datawarehouse;
 
 -- 2. Create the database
-CREATE DATABASE datawarehouse;
+CREATE DATABASE "DataWarehouse"
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'English_India.1252'
+    LC_CTYPE = 'English_India.1252'
+    LOCALE_PROVIDER = 'libc'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
 
 -- 3. IMPORTANT: You must connect to 'datawarehouse' before running the lines below.
 -- In psql, use: \c datawarehouse
